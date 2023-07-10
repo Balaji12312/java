5c7924fbe01578948127a4b329d23790cd7dd444
BALAJI <94888559+Balaji12312@users.noreply.github.com>
2023-07-10 18:55:22 +0530

    test

---
+++ cat3.java
@@ -0,0 +1,14 @@
+public class sum
+{
+public static void main(String Args[])
+{
+multiply 0=new multiply();
+int r=o.multiply()
+System.out.println(r);
+}
+public int multiply()
+{
+int a=4,int b=3;
+int c=a*b;
+return c;
+}
