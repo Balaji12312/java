90f6a575d278bf031683525d85a5956f9e23c3ef
BALAJI S <94888559+Balaji12312@users.noreply.github.com>
2023-07-18 09:57:37 +0530

    Add files via upload

---
+++ day6/All Digits Count.java
@@ -0,0 +1,5 @@
+class UserMainCode {
+    public int allDigitsCount(int input1) {
+        return String.valueOf(input1).length();
+    }
+}
---
+++ factorial of a number.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode
+{
+
+public int nFactorial(int input1){
+// Read only region end
+int i=1;
+int x=1;
+while(i<=input1){
+x=x*i;
+i++;
+}
+return x;
+
+}
+}
